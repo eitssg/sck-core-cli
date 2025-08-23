6355bbdd2d487fde154836bb536dc5de222cb769
James Barwick <jbarwick@me.com>
2025-08-23 10:10:45 +0800

    update constants

--- core_cli/info.py
+++ core_cli/info.py
@@ -46,14 +46,6 @@ from core_framework.constants import (
 )
 
 from core_db.config import get_table_name
-from core_db.constants import (
-    ITEMS,
-    EVENTS,
-    CLIENT_FACTS,
-    PORTFOLIO_FACTS,
-    ZONE_FACTS,
-    APP_FACTS,
-)
 
 from core_cli import __version__
 
@@ -85,18 +77,16 @@ def show_configuration(data):
     data[P_SCOPE] = scope_prefix = util.get_automation_scope() or ""
     data[P_AUTOMATION_ACCOUNT] = automation_account = util.get_automation_account()
 
-    data[P_ITEMS_TABLE_NAME] = items_table_name = get_table_name(ITEMS)
-    data[P_EVENTS_TABLE_NAME] = events_table_name = get_table_name(EVENTS)
-    data[P_CLIENT_TABLE_NAME] = clients_table = get_table_name(CLIENT_FACTS)
-    data[P_PORTFOLIOS_TABLE_NAME] = portfolios_table = get_table_name(PORTFOLIO_FACTS)
-    data[P_ZONES_TABLE_NAME] = zones_table = get_table_name(ZONE_FACTS)
-    data[P_APPS_TABLE_NAME] = apps_table = get_table_name(APP_FACTS)
+    data[P_ITEMS_TABLE_NAME] = items_table_name = get_table_name()
+    data[P_EVENTS_TABLE_NAME] = events_table_name = get_table_name()
+    data[P_CLIENT_TABLE_NAME] = clients_table = get_table_name()
+    data[P_PORTFOLIOS_TABLE_NAME] = portfolios_table = get_table_name()
+    data[P_ZONES_TABLE_NAME] = zones_table = get_table_name()
+    data[P_APPS_TABLE_NAME] = apps_table = get_table_name()
 
     data[P_BUCKET_REGION] = bucket_region = util.get_bucket_region()
     data[P_BUCKET_NAME] = automation_bucket = util.get_bucket_name(client)
-    data[P_ARTEFACT_BUCKET_NAME] = artefacts_bucket = util.get_artefact_bucket_name(
-        client
-    )
+    data[P_ARTEFACT_BUCKET_NAME] = artefacts_bucket = util.get_artefact_bucket_name(client)
     data[P_DYNAMODB_HOST] = dynamodb_host = util.get_dynamodb_host()
     data[P_DYNAMODB_REGION] = dynamodb_region = util.get_dynamodb_region()
     data[P_USE_S3] = util.is_use_s3()
@@ -163,9 +153,7 @@ def get_info_command(subparsers) -> ExecuteCommandsType:
 
     DESCRIPTION = "Display information about the core subsystem"
 
-    config_parser = subparsers.add_parser(
-        "info", description=DESCRIPTION, usage="core info", help=DESCRIPTION
-    )
+    config_parser = subparsers.add_parser("info", description=DESCRIPTION, usage="core info", help=DESCRIPTION)
     config_parser.set_group_title(0, "Configure actions")
     config_parser.set_group_title(1, "Available options")
 
